602e3a9e10db6aacd48a6f1dc0fa0c5dc1a1d2e1
miyaaa624 <a09837290595@gmail.com>
2024-12-16 12:59:47 +0800

    1216-照片和scss照片和scss

---
+++ funlab/src/js/script.js
@@ -0,0 +1,54 @@
+// 所有人共用  $(document).ready
+$(function () {
+
+    //漢堡按鈕 ⇩
+    $('.hamburger').click(function () {
+        //第二步驟 => 切換漢堡按鈕
+        $(this).toggleClass('is-active');
+        //第三步驟 => 出現show
+        $('.navigation').toggleClass('show');
+    });
+})
+
+//淡入淡出
+function animateFrom(elem, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if (elem.classList.contains("gs_reveal_fromLeft")) {
+        x = -100;
+        y = 0;
+    } else if (elem.classList.contains("gs_reveal_fromRight")) {
+        x = 100;
+        y = 0;
+    }
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+    gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
+        duration: 5,
+        x: 0,
+        y: 0,
+        autoAlpha: 1,
+        ease: "expo",
+        overwrite: "auto"
+    });
+}
+
+function hide(elem) {
+    gsap.set(elem, { autoAlpha: 0 });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+        hide(elem);
+
+        ScrollTrigger.create({
+            trigger: elem,
+            onEnter: function () { animateFrom(elem) },
+            onEnterBack: function () { animateFrom(elem, -1) },
+            onLeave: function () { hide(elem) }
+        });
+    });
+});
