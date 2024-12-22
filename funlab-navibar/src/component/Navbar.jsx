01c2fd3836d7f975b953fff31761460054fff43e
uhalw <uhalw95@gmail.com>
2024-12-23 00:54:32 +0800

    導覽列的sass匯入了，但不知道為何logo都不跟他display flex

--- funlab-navibar/src/component/Navbar.jsx
+++ funlab-navibar/src/component/Navbar.jsx
@@ -9,14 +9,14 @@ export default function Navbar() {
             </div>
             <div className={styles.navigation}>
                 <div className={styles.menu}>
-                    <Link className={styles.a_story} to="/about">關於放飲</Link>
-                    <Link className={styles.a_menu} to="/menu">飲品菜單</Link>
-                    <Link className={styles.a_fresh} to="/ingred">嚴選食材</Link>
-                    <Link className={styles.a_store} to="/store">門市據點</Link>
-                    <Link className={styles.a_search} to="/search">訂單查詢</Link>
+                    <Link className={styles.a_story} to="/about">ABOUT</Link>
+                    <Link className={styles.a_menu} to="/menu">MENU</Link>
+                    <Link className={styles.a_fresh} to="/ingred">FRESH</Link>
+                    <Link className={styles.a_store} to="/store">STORE</Link>
+                    <Link className={styles.a_search} to="/search">SEARCH</Link>
                 </div>
                 <div className={styles.funLab}>
-                    <img src="/images/funLogo.svg" alt="Funlab Logo" />
+                    <img src="/images/funLogo.svg" alt="FUNLAB" />
                 </div>
             </div>
         </div>
