e84fe8fb43c1582be8b188f6cfeaaba6f18ed291
“uhalw” <uhalw95@gmail.com>
2025-01-06 11:31:33 +0800

    0106 menu title

--- funlab-navibar/src/pages/Menu/Menu.jsx
+++ funlab-navibar/src/pages/Menu/Menu.jsx
@@ -4,10 +4,14 @@ import Tab from "./components/Tab";
 import "./Menu.scss";
 import Footer from "../../component/Footer";
 
+
 export default function Menu() {
     return (
         <div>
             <Banner /> 
+                        <header>
+                        <h1 className="page-title"> Menu </h1>
+                        </header>
             <Tab />
             <Footer/>
         </div>
