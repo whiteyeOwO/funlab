250acd25bbaf61e84ae01ea2b594c79403cd86d6
“uhalw” <uhalw95@gmail.com>
2025-01-13 15:00:22 +0800

    刪除檔案

--- funlab-navibar/src/pages/Menu/Menu.jsx
+++ funlab-navibar/src/pages/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import GSReveal from "../../component/GSReveal";
 import React from "react";
-import Banner from "./components/Banner";
+// import Banner from "./components/Banner";
 import Tab from "./components/Tab";
 import Footer from "../../component/Footer";
 import "./Menu.scss";
