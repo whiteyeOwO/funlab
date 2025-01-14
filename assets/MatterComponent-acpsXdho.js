979b2833a65d3f8b5359b6dcb6bc84d2c0f07feb
whiteyeOwO <funlabfunlab@gmail.com>
2025-01-14 17:01:18 +0800

    Updates

--- assets/MatterComponent-acpsXdho.js
+++ assets/MatterComponent-acpsXdho.js
@@ -1,4 +1,4 @@
-import{g as de,c as ie,r as j,j as V}from"./index-B9ZVSwVO.js";var ve={exports:{}};/*!
+import{g as de,c as ie,r as j,j as V}from"./index-DxDNee4_.js";var ve={exports:{}};/*!
  * matter-js 0.20.0 by @liabru
  * http://brm.io/matter-js/
  * License MIT
