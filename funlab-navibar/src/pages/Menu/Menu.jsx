493d8f902acc96e5612ce25de59a1a1632324d14
“uhalw” <uhalw95@gmail.com>
2025-01-16 10:26:51 +0800

    add menu graphs

--- funlab-navibar/src/pages/Menu/Menu.jsx
+++ funlab-navibar/src/pages/Menu/Menu.jsx
@@ -9,9 +9,16 @@ import "./Menu.scss";
 export default function Menu() {
     return (
         <div>
-                        <h1 className="page-title"><GSReveal from="top"> Menu </GSReveal> </h1>
+            <h1 className="page-title"><GSReveal from="top"> Menu </GSReveal> </h1>
             <Tab />
-            <Footer/>
+            {/* 裝飾 */}
+            <figure className="graph-1"><GSReveal><img src="images/icon/graph-pinkLine.svg" alt="" /> </GSReveal></figure>
+            <figure className="graph-2"><GSReveal><img src="images/icon/graph-pinkBall.svg" alt="" /> </GSReveal></figure>
+            <figure className="graph-3"><GSReveal><img src="images/icon/graph-yellowLine.svg" alt="" /> </GSReveal></figure>
+
+            <Footer />
+
+
         </div>
     );
 }
