3de7ead34e2bbc5bc2c5b9cc0212a3c3253cc7c2
whiteOwO <betty06231227@gmail.com>
2025-01-17 18:44:12 +0800

    0117

--- funlab-navibar/vite.config.js
+++ funlab-navibar/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     // base: process.env.NODE_ENV === 'production' ? '/funlab/' : '/',
-    base:"/", 
+    base:"./", 
     plugins: [react()],
     // css: {
     //     modules: {
